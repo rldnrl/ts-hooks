0e325ddd95dc8905d23a3857cb4749a12ca7275d
rldnrl <devpark1002@gmail.com>
2022-03-20 21:41:01 +0900

    feat: 유용한 룰 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,20 @@ module.exports = {
   ],
   rules: {
     'prettier/prettier': 'error',
+    'react/display-name': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+        selector: 'variable',
+        leadingUnderscore: 'allow',
+      },
+      { format: ['camelCase', 'PascalCase'], selector: 'function' },
+      { format: ['PascalCase'], selector: 'interface' },
+      { format: ['PascalCase'], selector: 'typeAlias' },
+    ],
   },
 }
