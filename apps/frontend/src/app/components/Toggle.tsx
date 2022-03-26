59e95255fe9b791ce1554d26a5ebe977b1c402b8
rldnrl <devpark1002@gmail.com>
2022-03-26 10:51:42 +0900

    feat: 예제 컴포넌트 구현

--- apps/frontend/src/app/components/Toggle.tsx
+++ apps/frontend/src/app/components/Toggle.tsx
@@ -1,7 +1,7 @@
 import { useToggle } from '@ts-hook/hooks'
 
 const Toggle = () => {
-  const [value, onToggle] = useToggle(false)
+  const { value, onToggle } = useToggle(false)
   return (
     <div>
       <p>{`${value}`}</p>
