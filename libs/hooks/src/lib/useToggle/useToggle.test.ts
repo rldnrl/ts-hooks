933612fc9702c7635935c40483dd72118ef268a6
rldnrl <devpark1002@gmail.com>
2022-03-26 10:51:24 +0900

    feat: test 코드 작성

---
+++ libs/hooks/src/lib/useToggle/useToggle.test.ts
@@ -0,0 +1,24 @@
+import { renderHook, act } from '@testing-library/react-hooks'
+import useToggle from './useToggle'
+
+describe('useToggle Test', () => {
+  it('should be toggle with initial value', () => {
+    const { result } = renderHook(() => useToggle(false))
+
+    act(() => {
+      result.current.onToggle()
+    })
+
+    expect(result.current.value).toBe(true)
+  })
+
+  it('should be toggle with empty value', () => {
+    const { result } = renderHook(() => useToggle())
+
+    act(() => {
+      result.current.onToggle()
+    })
+
+    expect(result.current.value).toBe(true)
+  })
+})
