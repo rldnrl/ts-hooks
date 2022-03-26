6ee537bbe5683e6be3f908ec916388d01ceab357
rldnrl <devpark1002@gmail.com>
2022-03-26 11:32:53 +0900

    feat: useCounter 테스트 코드 작성

---
+++ libs/hooks/src/lib/useCounter/useCounter.test.ts
@@ -0,0 +1,61 @@
+import { renderHook, act } from '@testing-library/react-hooks'
+import useCounter from './useCounter'
+
+describe('useCounter Test', () => {
+  it('should increase count', () => {
+    const { result } = renderHook(() => useCounter(0))
+
+    act(() => {
+      result.current.increase()
+      result.current.increase()
+      result.current.increase()
+      result.current.increase()
+      result.current.increase()
+    })
+
+    expect(result.current.count).toBe(5)
+  })
+  it('should decrease count', () => {
+    const { result } = renderHook(() => useCounter(0))
+
+    act(() => {
+      result.current.decrease()
+      result.current.decrease()
+      result.current.decrease()
+      result.current.decrease()
+      result.current.decrease()
+    })
+
+    expect(result.current.count).toBe(-5)
+  })
+  it('should reset count', () => {
+    const { result } = renderHook(() => useCounter(0))
+
+    act(() => {
+      result.current.decrease()
+      result.current.decrease()
+      result.current.decrease()
+      result.current.decrease()
+      result.current.decrease()
+
+      result.current.reset()
+    })
+
+    expect(result.current.count).toBe(0)
+  })
+  it('should increase and decrease count', () => {
+    const { result } = renderHook(() => useCounter(11))
+
+    act(() => {
+      result.current.decrease()
+      result.current.decrease()
+      result.current.decrease()
+      result.current.decrease()
+      result.current.decrease()
+
+      result.current.increase()
+    })
+
+    expect(result.current.count).toBe(7)
+  })
+})
