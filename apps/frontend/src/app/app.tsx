cdc6d0f8104e92c7c220ad13e3b1091fc9db0439
rldnrl <devpark1002@gmail.com>
2022-04-03 17:08:59 +0900

    feat: use update effect

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,11 +1,13 @@
 import Counter from './components/Counter'
 import Toggle from './components/Toggle'
+import UpdateEffect from './components/UpdateEffect'
 
 export function App() {
   return (
     <>
       <Toggle />
       <Counter />
+      <UpdateEffect />
     </>
   )
 }
