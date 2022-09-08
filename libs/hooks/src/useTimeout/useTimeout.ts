ba33c3fea09bcb52b2fcb7e1c7d4d63737178cb4
rldnrl <devpark1002@gmail.com>
2022-09-09 08:49:44 +0900

    feat: useTimeout

---
+++ libs/hooks/src/useTimeout/useTimeout.ts
@@ -0,0 +1,26 @@
+import {useEffect, useRef} from "react";
+import useIsomorphicLayoutEffect from "../useIsomorphicLayoutEffect";
+
+type UseTimeout = (callback: () => void, delay: number | null) => void
+
+const useTimeout: UseTimeout = (callback, delay) => {
+  const savedCallback = useRef(callback)
+
+  useIsomorphicLayoutEffect(() => {
+    savedCallback.current = callback
+  }, [callback])
+
+  useEffect(() => {
+    if (!delay && delay !== 0) {
+      return
+    }
+
+    const timer = setTimeout(() => savedCallback.current(), delay)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [delay])
+}
+
+export default useTimeout
